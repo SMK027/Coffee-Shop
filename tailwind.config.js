24ec10284f2495f738964b92c1ad6ba86ea0b367
Léo FRANZ <leofranz2006@gmail.com>
2026-09-03 22:11:18 +0200

    feat(tailwind): ajouter une liste de sécurité pour les classes de style

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,6 +9,19 @@ export default {
         './resources/views/**/*.blade.php',
     ],
 
+    safelist: [
+        'bg-stone-600', 'hover:bg-stone-500', 'bg-stone-800', 'hover:bg-stone-700',
+        'bg-amber-600', 'hover:bg-amber-500',
+        'bg-blue-600', 'hover:bg-blue-500',
+        'bg-green-600', 'hover:bg-green-500',
+        'bg-red-100', 'hover:bg-red-200', 'text-red-700',
+        'bg-purple-600', 'hover:bg-purple-500',
+        'bg-indigo-600', 'hover:bg-indigo-500',
+        'bg-orange-600', 'hover:bg-orange-500',
+        'bg-teal-600', 'hover:bg-teal-500',
+        'text-white',
+    ],
+
     theme: {
         extend: {
             fontFamily: {
